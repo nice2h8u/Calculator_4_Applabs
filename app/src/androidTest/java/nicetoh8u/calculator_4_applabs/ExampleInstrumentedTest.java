5cd257eb4bdc4438e75340a721544f0a2a6c173e
nice2h8u <nicetoh8u@gmail.com>
2018-08-02 19:35:33 +0300

    All tests added

--- app/src/androidTest/java/nicetoh8u/calculator_4_applabs/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/nicetoh8u/calculator_4_applabs/ExampleInstrumentedTest.java
@@ -1,32 +1,224 @@
 package nicetoh8u.calculator_4_applabs;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
 
+
+
+
+import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
-import static org.junit.Assert.*;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
+
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
 
 
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+    public void checkDots() {
+        onView(withId(R.id.button_0))
+                .perform(click());
+        onView(withId(R.id.button_0))
+                .perform(click());
+        onView(withId(R.id.button_0))
+                .perform(click());
+        onView(withId(R.id.button_dot))
+                .perform(click());
+        onView(withId(R.id.button3))
+                .perform(click());
+        onView(withId(R.id.button_plus))
+                .perform(click());
+        onView(withId(R.id.button3))
+                .perform(click());
+        onView(withId(R.id.button_result))
+                .perform(click());
+        onView(withId(R.id.calculate_area))
+                .check(matches(withText("3.30")));
 
-        assertEquals("nicetoh8u.calculator_4_applabs", appContext.getPackageName());
     }
 
-    //@Test
-  //  public void check
+    @Test
+    public void checkPlusOperation() {
+        onView(withId(R.id.button))
+                .perform(click());
+        onView(withId(R.id.button_dot))
+                .perform(click());
+        onView(withId(R.id.button))
+                .perform(click());
+        onView(withId(R.id.button_plus))
+                .perform(click());
+        onView(withId(R.id.button))
+                .perform(click());
+        onView(withId(R.id.button_result))
+                .perform(click());
+        onView(withId(R.id.calculate_area))
+                .check(matches(withText("2.10")));
+
+    }
+
+    @Test
+    public void checkMinusOperation() {
+        onView(withId(R.id.button))
+                .perform(click());
+        onView(withId(R.id.button_dot))
+                .perform(click());
+        onView(withId(R.id.button))
+                .perform(click());
+        onView(withId(R.id.button_minus))
+                .perform(click());
+        onView(withId(R.id.button))
+                .perform(click());
+        onView(withId(R.id.button_result))
+                .perform(click());
+        onView(withId(R.id.calculate_area))
+                .check(matches(withText("0.10")));
+
+    }
+
+    @Test
+    public void checkMultiOperation() {
+        onView(withId(R.id.button))
+                .perform(click());
+        onView(withId(R.id.button_dot))
+                .perform(click());
+        onView(withId(R.id.button))
+                .perform(click());
+        onView(withId(R.id.button_multi))
+                .perform(click());
+        onView(withId(R.id.button))
+                .perform(click());
+        onView(withId(R.id.button_result))
+                .perform(click());
+        onView(withId(R.id.calculate_area))
+                .check(matches(withText("1.10")));
+
+    }
+    @Test
+    public void checkDivOperation() {
+        onView(withId(R.id.button8))
+                .perform(click());
+        onView(withId(R.id.button_dot))
+                .perform(click());
+        onView(withId(R.id.button8))
+                .perform(click());
+        onView(withId(R.id.button_div))
+                .perform(click());
+        onView(withId(R.id.button4))
+                .perform(click());
+        onView(withId(R.id.button_result))
+                .perform(click());
+        onView(withId(R.id.calculate_area))
+                .check(matches(withText("2.20")));
+
+    }
+
+    @Test
+    public void checkClickOnNumbers() {
+
+
+        onView(withId(R.id.button))
+                .perform(click());
+        onView(withId(R.id.button2))
+                .perform(click());
+        onView(withId(R.id.button3))
+                .perform(click());
+        onView(withId(R.id.button4))
+                .perform(click());
+        onView(withId(R.id.button5))
+                .perform(click());
+        onView(withId(R.id.button_plus))
+                .perform(click());
+        onView(withId(R.id.button6))
+                .perform(click());
+        onView(withId(R.id.button7))
+                .perform(click());
+        onView(withId(R.id.button8))
+                .perform(click());
+        onView(withId(R.id.button9))
+                .perform(click());
+        onView(withId(R.id.button_0))
+                .perform(click());
+        onView(withId(R.id.button_result))
+                .perform(click());
+
+       // check that TextView contains error text
+        onView(withId(R.id.calculate_area))
+                .check(matches(withText("80235")));
+    }
+
+    @Test
+    public void tryToFindErrors(){
+        onView(withId(R.id.button_minus))
+                .perform(click());
+        onView(withId(R.id.button_dot))
+                .perform(click());
+        onView(withId(R.id.button8))
+                .perform(click());
+        onView(withId(R.id.button_delete))
+                .perform(click()).perform(click()).perform(click());
+        onView(withId(R.id.button8))
+                .perform(click());
+        onView(withId(R.id.button_div))
+                .perform(click());
+        onView(withId(R.id.button_multi))
+                .perform(click());
+        onView(withId(R.id.button9))
+                .perform(click());
+        onView(withId(R.id.button_dot))
+                .perform(click());
+        onView(withId(R.id.button_result))
+                .perform(click());
+        onView(withId(R.id.button_minus))
+                .perform(click());
+        onView(withId(R.id.button_plus))
+                .perform(click());
+        onView(withId(R.id.button_div))
+                .perform(click());
+        onView(withId(R.id.button_multi))
+                .perform(click());
+        onView(withId(R.id.button))
+                .perform(click());
+        onView(withId(R.id.button_div))
+                .perform(click());
+
+        // check that TextView contains error text
+        onView(withId(R.id.calculate_area))
+                .check(matches(withText("72.80/")));
+    }
+
+    @Test
+    public void checkCancelButton(){
+
+        onView(withId(R.id.button_plus))
+                .perform(click());
+        onView(withId(R.id.button_minus))
+                .perform(click());
+        onView(withId(R.id.button))
+                .perform(click());
+        onView(withId(R.id.button_multi))
+                .perform(click());
+        onView(withId(R.id.button_0))
+                .perform(click());
+        onView(withId(R.id.button_div))
+                .perform(click());
+        onView(withId(R.id.button_cancel))
+                .perform(click());
+
+        // check that TextView contains error text
+        onView(withId(R.id.calculate_area))
+                .check(matches(withText("")));
+    }
+
 }
